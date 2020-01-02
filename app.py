3dedee5a882b20829da81074abdd398d2a960067
Ubuntu <ubuntu@ip-172-31-34-153.ec2.internal>
2020-01-02 15:47:57 +0000

    Added tasks.html and mongoDB connection

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'Task_Manager'
+app.config["MONGO_URI"] = 'mongodb+srv://rootbeer:r00tb33r@myfirstcluster-utt5o.mongodb.net/Task_Manager?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
